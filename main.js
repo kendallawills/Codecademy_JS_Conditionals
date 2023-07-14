2a4a60a620017572fe64f7c8157f2144386cff73
Kendall <kendallawills@gmail.com>
2023-07-14 14:44:47 -0400

    Switch Keyword

--- main.js
+++ main.js
@@ -85,3 +85,21 @@ if (season === 'spring') {
 } else {
     console.log('Invalid season.');
 }
+
+// Switch Keyword
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+    case 'first place':
+        console.log('You get the gold medal!');
+        break;
+    case 'second place':
+        console.log('You get the silver medal!');
+        break;
+    case 'third place':
+        console.log('You get the bronze medal!');
+        break;
+    default:
+        console.log('No medal awarded.');
+        break;
+}
