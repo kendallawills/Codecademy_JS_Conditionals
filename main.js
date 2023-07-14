52283ec17e33c549d78012824224f54b6aa5d81c
Kendall <kendallawills@gmail.com>
2023-07-14 13:49:54 -0400

    Truthy Falsy

--- main.js
+++ main.js
@@ -33,3 +33,21 @@ if (mood === 'sleepy' && tirednessLevel > 8) {
 } else {
     console.log('not bed time yet');
 }
+
+// Truthy Falsy
+// Truthy
+let wordCount = 2;
+
+if (wordCount) {
+    console.log("Great! You've started your work!");
+} else {
+    console.log('Better get to work!');
+}
+// Falsy
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+    console.log("This string doesn't seem to be empty.");
+} else {
+    console.log('This string is definitely empty.');
+}
