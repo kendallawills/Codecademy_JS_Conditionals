3d574cdc24cd96750f27ffde4cab44344944940d
Kendall <kendallawills@gmail.com>
2023-07-14 13:33:46 -0400

    If...Else Statements

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ let sale = true;
 if (sale) {
     console.log('Time to buy!');
 }
+
+// If...Else Statements
+let sales = true;
+sales = false;
+
+if (sales) {
+    console.log('Time to buy!');
+} else {
+    console.log('Time to wait for a sale.');
+}
