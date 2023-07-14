dc517e811dd46f53dd2cc39d19057248455fac71
Kendall <kendallawills@gmail.com>
2023-07-14 13:37:18 -0400

    Comparison Operators

--- main.js
+++ main.js
@@ -14,3 +14,12 @@ if (sales) {
 } else {
     console.log('Time to wait for a sale.');
 }
+
+//Comparison Operators
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+    console.log('Time to eat!');
+} else {
+    console.log('We can eat later!');
+}
